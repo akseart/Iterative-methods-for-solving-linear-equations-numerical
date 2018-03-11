6823324e9aef3cb3a77caada79e26a7009132189
akseart <akseart@ya.ru>
2018-03-11 15:32:06 +0300

    Initial Commit

---
+++ Iterative methods for solving linear equations numerical/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Iterative methods for solving linear equations numerical
+//
+//  Created by Артем on 11.03.2018.
+//  Copyright © 2018 Артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
